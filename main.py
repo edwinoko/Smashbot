67ffd76b5c4a6092b750d8c6403e58f5434855ef
edwinoko <edwinoko@yahoo.com>
2021-08-19 13:39:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import discord
+import os
+import requests
+import json
+import random
+
+client = discord.Client()
+
+TOKEN=''
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+client.run(TOKEN)
