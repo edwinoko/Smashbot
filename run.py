092cfa4f487e4e3febeb44a41ffd69cf82128241
Edwin Agyemang <edwinagyemang@Edwins-Air.home>
2021-08-25 11:06:22 +0200

    added functionality

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
+from wrapper import handle_command
+from random_quotes.quote import get_random_quote
 import discord
 import os
+import discord
+from dotenv import load_dotenv
 
 client = discord.Client()
 
-TOKEN = os.getenv('TOKEN')
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 @client.event
 async def on_ready():
@@ -11,7 +16,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.content.startwith('$smash'):
+  if message.content.startswith('$smash'):
     await message.channel.send('we smashin bro')
 
   if message.content.startswith('$hello'):
@@ -19,7 +24,24 @@ async def on_message(message):
 
   if message.content.startswith("shrug"):
     await message.channel.send('¯\_(ツ)_/¯')
+
+# sends a random gamer quote
+  if message.content.startswith("gg"):
+    await message.channel.send(get_random_quote())
+
+# get frame data of a character
+  if message.content.startswith('!smash'):
+    list_of_lines = handle_command(message.content)
+    # send string to the function to analyze the smash command.
+    if type(list_of_lines) is list:
+      for line in list_of_lines:
+        await message.channel.send(line)
+    if type(list_of_lines) is str:
+      await message.channel.send(list_of_lines)
   
   if message.author == client.user:
     return
+  
+
 client.run(TOKEN)
+
